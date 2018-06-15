2a8d597710097d931fd38e190c66c27b665a199b
Dathan Spencer <dpspencer88@gmail.com>
2018-06-14 19:53:30 -0600

    Get JS math functions working

--- calculator.js
+++ calculator.js
@@ -1,5 +1,65 @@
-function sum() {
-	document.getElementById('display').value = 
-			document.getElementById('btn').value;
+function add(numbers) {
+	let sum = 0;
+	for (i = 0; i < numbers.length; i++) {
+		sum = sum + numbers[i];
+	};
+	return sum;
 }
+
+function subtract(numbers) {
+	let sum = 0;
+	for (i = 0; i < numbers.length; i++) {
+		sum = sum + numbers[i];
+	};
+	return sum;
+}
+
+function multiply(numbers) {
+	let sum = 1;
+	for (i = 0; i < numbers.length; i++) {
+		sum = sum * numbers[i];
+	};
+	return sum;
+}
+
+function divide(numbers) {
+	let sum = 1;
+	for (i = 0; i < numbers.length; i++) {
+		sum = sum / numbers[i];
+	};
+	return sum;
+}
+
+function operate() {
+	let a = Math.floor(Math.random() * 100);
+	let b = Math.floor(Math.random() * 100);
+	let op = ['+', '-', '*', '/'];
+	let sign = op[Math.floor(op.length * Math.random())];
+	let final = a + sign + b;
+
+	console.log(a, b, sign, final);
+
+	if(sign === '+') {
+		return add([a, b]);
+		console.log();
+	} else if(sign === '-') {
+		return subtract([a, -b]);
+		console.log();
+	} else if(sign === '*') {
+		return multiply([a, b]);
+		console.log();
+	} else if (sign === '/'){
+		return divide([a, b]);
+	} else {
+		return "Is not a number";
+	}
+}
+
+function submit() {
+
+}
+
+console.log(operate());
+
+console.log(submit());
 

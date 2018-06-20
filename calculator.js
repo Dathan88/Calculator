b6c9c077649c3736d83ba2d804e32e0e9ebfeeae
Dathan Spencer <dpspencer88@gmail.com>
2018-06-20 16:09:41 -0600

    Try to make functions to handle input

--- calculator.js
+++ calculator.js
@@ -36,7 +36,7 @@ function randomMath() {
 	let sign = ['+', '-', '*', '/'];
 	let randomOp = op[Math.floor(op.length * Math.random())];
 	let final = a + sign + b;
-	console.log(a, b, sign, final); // Function used to test different operators and accuracy
+	console.log(a, b, sign, final);  // Function used to test different operators and accuracy
 
 	if(sign === '+') {
 		return add([a, b]);
@@ -49,7 +49,8 @@ function randomMath() {
 	}
 }
 
-//------ attachments of buttons to calculator and appearing in display
+//-- above -- functions that process math problems and find correct values
+//-- below -- functions/variables that combine the above with web calculator app
 
 const btn = document.querySelectorAll('button');
 const display = document.getElementById('display');
@@ -57,32 +58,28 @@ let displayArray = [];
 
 btn.forEach((button) => {
   button.addEventListener('click', (e) => {
-   displayArray.push(e.target.value);
+   displayArray.push(e.target.value);	// each click puts button value into display 
 	display.value = displayArray.join('');
   });
 });
 
-function erase() {
-	let length = displayArray.length;
-	for(i = length; i > 0; i--) {
-		displayArray.pop([i]);
-		document.getElementById('display').value = displayArray;
-	}
+
+function operate(e) {
+	let sign = event.target.value;
+	let firstNum = display.value += sign;
+
+
+	console.log(firstNum);
+
 }
 
-function answers() {
-	let problem = display.value;
 
-	let sign = ['+', '-', '*', '/'];
+function erase() {
+	let length = displayArray.length;
 
-	if(sign === '+') {
-		return add([a, b]);
-	} else if(sign === '-') {
-		return subtract([a, -b]);
-	} else if(sign === '*') {
-		return multiply([a, b]);
-	} else if (sign === '/') {
-		return divide([a, b]);
+	for(i = length; i > 0; i--) {  	// permanently resets display
+		displayArray.pop([i]);
+		display.value = displayArray;
 	}
 }
 

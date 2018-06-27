f0c8ddb58b4f9c0ad89a40b345d5777bafb8797a
Dathan Spencer <dpspencer88@gmail.com>
2018-06-26 19:43:08 -0600

    Still trying to figure out equals button

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
+function randomMath() {
+	let a = Math.floor(Math.random() * 100);
+	let b = Math.floor(Math.random() * 100);
+	let sign = ['+', '-', '*', '/'];
+	let randomOp = op[Math.floor(op.length * Math.random())];
+	let final = a + sign + b;
+	console.log(a, b, sign, final);  // Function used to test different operators and accuracy
+
+	if(sign === '+') {
+		return add([a, b]);
+	} else if(sign === '-') {
+		return subtract([a, -b]);
+	} else if(sign === '*') {
+		return multiply([a, b]);
+	} else if (sign === '/') {
+		return divide([a, b]);
+	}
+}
+
 function add(numbers) {
 	let sum = 0;
 	for (i = 0; i < numbers.length; i++) {
@@ -9,7 +28,7 @@ function add(numbers) {
 function subtract(numbers) {
 	let sum = 0;
 	for (i = 0; i < numbers.length; i++) {
-		sum = sum + numbers[i];
+		sum = sum -= numbers[i];
 	};
 	return sum;
 }
@@ -30,25 +49,6 @@ function divide(numbers) {
 	return sum;
 }
 
-function randomMath() {
-	let a = Math.floor(Math.random() * 100);
-	let b = Math.floor(Math.random() * 100);
-	let sign = ['+', '-', '*', '/'];
-	let randomOp = op[Math.floor(op.length * Math.random())];
-	let final = a + sign + b;
-	console.log(a, b, sign, final);  // Function used to test different operators and accuracy
-
-	if(sign === '+') {
-		return add([a, b]);
-	} else if(sign === '-') {
-		return subtract([a, -b]);
-	} else if(sign === '*') {
-		return multiply([a, b]);
-	} else if (sign === '/') {
-		return divide([a, b]);
-	}
-}
-
 //-- above -- functions that process math problems and find correct values
 //-- below -- functions/variables that combine the above with web calculator app
 
@@ -58,9 +58,14 @@ const myOp = document.getElementById('signs');
 const myNumber = document.getElementById('number');
 let subArray = [];
 let displayArray = [];
+let problem = {
+	a: '',
+	sign: '',
+	b: '',
+	fullProblem: function() {return this.a + this.sign + this.b;}
+};
 
-
-function numbers(e) {
+function numbers() {
 	let number = event.target.value;
 		displayArray.push(number);
 		display.value = displayArray.join('');
@@ -68,71 +73,44 @@ function numbers(e) {
 	console.log(display.value);
 }
 
-function operators(e) {
+function operators() {
 	subArray.push(display.value);
-	let sign = event.target.value;
-		subArray.push(sign);
 
-	let a = subArray[0];
-	let b = subArray[2];
-	let final = a + sign + b;
-		
-	if(sign === '=') {
-		
-	}
-
-	if(sign === '+') {
-			return add([a, b]);
-		} else if(sign === '-') {
-			return subtract([a, -b]);
-		} else if(sign === '*') {
-			return multiply([a, b]);
-		} else if (sign === '/') {
-			return divide([a, b]);
-		}
-
-	subDisplay.innerHTML = subArray.join(' ');
-		displayArray = [''];
+	let op = event.target.value;
+		subArray.push(op);
+		subDisplay.innerHTML = subArray.join(' ');
+	displayArray = [''];
 
 	console.log(subArray);
 	console.log(subDisplay);
 }
 
+function solve() {
+	subArray.push(display.value);
+	subDisplay.innerHTML = subArray.join(' ') + " = ";
 
-/*
-function equals() {
-	subArray.push("=");
-		subDisplay.innerHTML = subArray.join(' ');
-
-	if(sign === '+') {
-		return add([a, b]);
-	} else if(sign === '-') {
-		return subtract([a, -b]);
-	} else if(sign === '*') {
-		return multiply([a, b]);
-	} else if (sign === '/') {
-		return divide([a, b]);
+	if(problem.sign === '+') {
+		return add(a, b);
+	} else if(problem.sign === '-') {
+		return subtract(a, -b);
+	} else if(problem.sign === '*') {
+		return multiply(a, b);
+	} else if (problem.sign === '/') {
+		return divide(a, b);
 	}
-console.log(subArray);
+
+	console.log(problem); 
 }
-*/
 
 function erase() {
 	let length = displayArray.length;
 	
-	for(i = length; i > 0; i--) {  			// permanently resets display
+	for(i = length; i > 0; i--) {  	// permanently resets display
 		displayArray.pop([i]);
+		subArray = [];
 		display.value = displayArray;
+		subDisplay.innerHTML = "Hello again!";
 	}
 };
 
-
-
-/*
- btn.forEach((button) => {
-  button.addEventListener('click', (e) => {
-   displayArray.push(e.target.value);	// each click puts button value into display 
-	display.value = displayArray.join('');
-  });
-});
-*/
+console.log(problem.fullProblem());

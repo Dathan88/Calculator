8ec763ba35b98c3715e40669eed86342354a7cf6
Dathan Spencer <dpspencer88@gmail.com>
2018-06-28 19:52:33 -0600

    Work out math/display

--- calculator.js
+++ calculator.js
@@ -1,116 +1,148 @@
+const display = document.getElementById('display');
+const subDisplay = document.getElementById('subDisplay');
+const myOp = document.getElementById('signs');
+const myNumber = document.getElementById('number');
+let topDisplay = [];
+let mainDisplay = [];
+
+
+function myNumbers(a) {
+	let number = event.target.value;
+		mainDisplay.push(number);
+
+		display.value = mainDisplay.join('');
+
+	console.log(display.value);
+}
+
+function operators(b) {
+	topDisplay.push(display.value);
+
+	let op = event.target.value;
+		topDisplay.push(op);
+		subDisplay.innerHTML = topDisplay.join(' ');
+
+	mainDisplay = [''];
+
+	console.log(mainDisplay);
+	console.log(subDisplay);
+}
+
+function equals(c) {
+	topDisplay.push(display.value);
+	topDisplay.push(' = ')
+	subDisplay.innerHTML = topDisplay.join(' ');
+
+	let a = parseInt(topDisplay[0]);
+	let sign = topDisplay[1];
+	let b = parseInt(topDisplay[2]);
+	let problem = a + sign + b;
+
+	console.log(a, b);
+	console.log(problem);
+
+			function answer(a, b) {
+				if(sign === '+') {
+			return add([a, b]);
+		} else if(sign === '-') {
+			return subtract([a, -b]);
+		} else if(sign === '*') {
+			return multiply([a, b]);
+		} else if (sign === '/') {
+			return divide([a, b]);
+		}
+	
+	};
+
+	answer(a, b);
+	console.log(topDisplay);
+};
+
+
+function erase() {
+	let length = mainDisplay.length;
+	
+	for(i = length; i > 0; i--) {  	// permanently resets display
+		mainDisplay.pop([i]);
+		topDisplay = [];
+		display.value = mainDisplay;
+		subDisplay.innerHTML = "Hello again!";
+	}
+};
+
 function randomMath() {
+	let length = mainDisplay.length;
+	
+	for(i = length; i > 0; i--) {  	// permanently resets display
+		mainDisplay.pop([i]);
+		topDisplay = [];
+		display.value = mainDisplay;
+		subDisplay.innerHTML = "Hello again!";
+	}
+
 	let a = Math.floor(Math.random() * 100);
 	let b = Math.floor(Math.random() * 100);
-	let sign = ['+', '-', '*', '/'];
+	let op = ['+', '-', '*', '/'];
 	let randomOp = op[Math.floor(op.length * Math.random())];
-	let final = a + sign + b;
-	console.log(a, b, sign, final);  // Function used to test different operators and accuracy
+	let final = a + randomOp + b;
+	console.log(a, b, randomOp, final);  // Function used to test different operators and accuracy
 
-	if(sign === '+') {
+	subDisplay.innerHTML = a + " " + randomOp + " " + b;
+
+	if(randomOp === '+') {
 		return add([a, b]);
-	} else if(sign === '-') {
+	} else if(randomOp === '-') {
 		return subtract([a, -b]);
-	} else if(sign === '*') {
+	} else if(randomOp === '*') {
 		return multiply([a, b]);
-	} else if (sign === '/') {
+	} else if (randomOp === '/') {
 		return divide([a, b]);
 	}
+	
 }
 
 function add(numbers) {
 	let sum = 0;
 	for (i = 0; i < numbers.length; i++) {
-		sum = sum + numbers[i];
+		sum += numbers[i];
 	};
+	mainDisplay.push(sum);
+	display.value = mainDisplay.join('');
+	console.log(sum);
 	return sum;
 }
 
 function subtract(numbers) {
 	let sum = 0;
 	for (i = 0; i < numbers.length; i++) {
-		sum = sum -= numbers[i];
+		sum += numbers[i];
 	};
+	mainDisplay.push(sum);
+	display.value = mainDisplay.join('');
+	console.log(sum);
 	return sum;
 }
 
 function multiply(numbers) {
 	let sum = 1;
 	for (i = 0; i < numbers.length; i++) {
-		sum = sum * numbers[i];
+		sum *= numbers[i];
 	};
+	mainDisplay.push(sum);
+	display.value = mainDisplay.join('');
+	console.log(sum);
 	return sum;
 }
 
 function divide(numbers) {
 	let sum = 1;
 	for (i = 0; i < numbers.length; i++) {
-		sum = sum / numbers[i];
+		sum /= numbers[i];
 	};
+	mainDisplay.push(sum);
+	display.value = mainDisplay.join('');
+	console.log(sum);
 	return sum;
 }
-
-//-- above -- functions that process math problems and find correct values
-//-- below -- functions/variables that combine the above with web calculator app
-
-const display = document.getElementById('display');
-const subDisplay = document.getElementById('subDisplay');
-const myOp = document.getElementById('signs');
-const myNumber = document.getElementById('number');
-let subArray = [];
-let displayArray = [];
-let problem = {
-	a: '',
-	sign: '',
-	b: '',
-	fullProblem: function() {return this.a + this.sign + this.b;}
-};
-
-function numbers() {
-	let number = event.target.value;
-		displayArray.push(number);
-		display.value = displayArray.join('');
-
-	console.log(display.value);
-}
-
-function operators() {
-	subArray.push(display.value);
-
-	let op = event.target.value;
-		subArray.push(op);
-		subDisplay.innerHTML = subArray.join(' ');
-	displayArray = [''];
-
-	console.log(subArray);
-	console.log(subDisplay);
-}
-
-function solve() {
-	subArray.push(display.value);
-	subDisplay.innerHTML = subArray.join(' ') + " = ";
-
-	if(problem.sign === '+') {
-		return add(a, b);
-	} else if(problem.sign === '-') {
-		return subtract(a, -b);
-	} else if(problem.sign === '*') {
-		return multiply(a, b);
-	} else if (problem.sign === '/') {
-		return divide(a, b);
-	}
-
-	console.log(problem); 
-}
-
-function erase() {
-	let length = displayArray.length;
-	
-	for(i = length; i > 0; i--) {  	// permanently resets display
-		displayArray.pop([i]);
-		subArray = [];
-		display.value = displayArray;
-		subDisplay.innerHTML = "Hello again!";
-	}
-};
-
-console.log(problem.fullProblem());
+randomMath();
+console.log();

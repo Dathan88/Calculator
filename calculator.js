d0187014f028ebd38a0794607079697905cc345b
Dathan Spencer <dpspencer88@gmail.com>
2018-07-05 20:35:02 -0600

    Getting closer

--- calculator.js
+++ calculator.js
@@ -2,16 +2,15 @@ const display = document.getElementById('display');
 const subDisplay = document.getElementById('subDisplay');
 const myOp = document.getElementById('signs');
 const myNumber = document.getElementById('number');
-let x;
-let signCount = 0;
-let topDisplay = [];
-let mainDisplay = [];
+var x;
+var signCount = 0;
+var topDisplay = [];
+var mainDisplay = [];
 
 function myNumbers(a) {
 	let number = event.target.value;
 		mainDisplay.push(number);
 		display.value = mainDisplay.join('');
-
 	console.log(display.value);
 }
 
@@ -37,59 +36,101 @@ function operators(b) {
 	mainDisplay = [''];
 }
 
-function equals() {									//called from operators function when "=" button pressed
-		let i, a, b, c;									//no for...in loops. Stop asking;
-	answer:
-	for (i = 0; i < topDisplay.length; i++) {
-		if(i > signCount) {
-			break answer;
-		} else {
-		switch(topDisplay[i]) {
-			case "*":
-				a = parseInt(topDisplay[i - 1]);
-				b = parseInt(topDisplay[i + 1]);
-				
-					console.log(a, b);
-				multiply([a, b]);
-			case "/":
-				a = parseInt(topDisplay[i - 1]);
-				b = parseInt(topDisplay[i + 1]);
-		
-					console.log(a, b);
-				divide([a, b]);
-			case "+":
-				a = parseInt(topDisplay[i - 1]);
-				b = parseInt(topDisplay[i + 1]);
-					console.log(a, b);
-				add([a, b]);
-				topDisplay.splice(i - 1, 3);
-			case "-":
-				a = parseInt(topDisplay[i - 1]);
-				b = parseInt(topDisplay[i + 1]);
-		
-					console.log(a, b);
-				subtract([a, b]);
-			default:
-				"Error"
-				break;
+function equals() {				//no for...in loops. Stop asking;	Called from operators function when "=" button pressed
+	var i, a, b;
+	var args;
+	if(topDisplay.length > 1) {
+		for (i = 0; i < topDisplay.length; i++) {
+			switch(topDisplay[i]) {
+				case "*":
+					a = parseInt(topDisplay[i - 1]);
+					b = parseInt(topDisplay[i + 1]);
+						console.log(a, b);
+						multiply([a, b]);
+					topDisplay.splice(i - 1, 3, x);
+					break;
+				case "/":
+					a = parseInt(topDisplay[i - 1]);
+					b = parseInt(topDisplay[i + 1]);
+						console.log(a, b);
+						divide([a, b]);
+					topDisplay.splice(i - 1, 3, x);
+					break;
+				case "+":
+					a = Number(topDisplay[i - 1]);
+					b = Number(topDisplay[i + 1]);
+						add([a, b]);
+						console.log(x);
+						console.log(a, b);
+					topDisplay.splice(i - 1, 3, x);
+						break;
+				case "-":
+					a = parseInt(topDisplay[i - 1]);
+					b = parseInt(topDisplay[i + 1]);
+						console.log(a, b);
+						subtract([a, b]);
+					topDisplay.splice(i - 1, 3, x);
+					break;
+				default:
+					"Error"
+					break;
+				}
 			}
-		}
-		console.log(topDisplay[i]);
+		console.log(topDisplay);
+		console.log(i);
+		console.log(x);
 	}
-	console.log(topDisplay);
-	console.log(signCount);
 };
 
 function erase() {
 	let length = mainDisplay.length;
-	
-	for(i = length; i > 0; i--) {  	// permanently resets display
+	for(i = length; i > 0; i--) {  			// permanently resets display
 		mainDisplay.pop([i]);
 		topDisplay = [];
 		display.value = mainDisplay;
 		subDisplay.innerHTML = "Hello again!";
 	}
 };
+
+function add(numbers) {
+	x = 0;
+	for (i = 0; i < numbers.length; i++) {
+		x += numbers[i];
+	};
+	console.log(x);
+	display.value = x;
+	return x;
+}
+
+function subtract(numbers) {
+	x = 0;
+	for (i = 0; i < numbers.length; i++) {
+		x += numbers[i];
+	};
+	console.log(x);
+	display.value = x;
+	return x;
+}
+
+function multiply(numbers) {
+	x = 1;
+	for (i = 0; i < numbers.length; i++) {
+		x *= numbers[i];
+	};
+	console.log(x);
+	display.value = x;
+	return x;
+}
+
+function divide(numbers) {
+	x = 1;
+	for (i = 0; i < numbers.length; i++) {
+		x /= numbers[i];
+	};
+	console.log(x);
+	display.value = x;
+	return x;
+}
 
 function randomMath() {									// Function used to test different operators and accuracy
 	let length = mainDisplay.length;
@@ -99,15 +140,12 @@ function randomMath() {									// Function used to test different operators and
 		display.value = mainDisplay;
 		subDisplay.innerHTML = "Hello again!";
 	}
-
 	let a = Math.floor(Math.random() * 100);
 	let b = Math.floor(Math.random() * 100);
 	let op = ['+', '-', '*', '/'];
 	let randomOp = op[Math.floor(op.length * Math.random())];
 	let final = a + randomOp + b;
-
-	subDisplay.innerHTML = a + " " + randomOp + " " + b;
-
+		subDisplay.innerHTML = a + " " + randomOp + " " + b;
 	if(randomOp === '+') {
 		return add([a, b]);
 	} else if(randomOp === '-') {
@@ -118,44 +156,6 @@ function randomMath() {									// Function used to test different operators and
 		return divide([a, b]);
 	}
 }
-
-function add(numbers) {
-	x = 0;
-	for (i = 0; i < numbers.length; i++) {
-		x += numbers[i];
-	};
-	console.log(x);
-	return x;
-}
-
-function subtract(numbers) {
-	x = 0;
-	for (i = 0; i < numbers.length; i++) {
-		x += numbers[i];
-	};
-	console.log(x);
-	return x;
-}
-
-function multiply(numbers) {
-	x = 1;
-	for (i = 0; i < numbers.length; i++) {
-		x *= numbers[i];
-	};
-	console.log(x);
-	return x;
-}
-
-function divide(numbers) {
-	x = 1;
-	for (i = 0; i < numbers.length; i++) {
-		x /= numbers[i];
-	};
-	console.log(x);
-	return x;
-}
-
-console.log();
 
 /*
 let sign = {
@@ -171,7 +171,5 @@ let sign = function() {
 
 			}
 		}
-
-
 
 */
